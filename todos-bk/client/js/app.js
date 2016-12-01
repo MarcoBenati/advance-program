a6e419d782262ca5b3baf0421f08dcbb5d31e464
Filippo Gaudenzi <gaudenzi.filippo@gmail.com>
2016-12-01 15:33:55 +0100

    commit update

--- todos-bk/client/js/app.js
+++ todos-bk/client/js/app.js
@@ -1,7 +1,33 @@
 var app = angular.module('app1',['lbServices']);
 
 app.controller('app1Controller',
-  ['$scope',function($scope){
+  ['$scope','Todos',function($scope,Todos){
+      $scope.newTodo={
+        name:"",
+        desc:"",
+        important:false
+      }
+      $scope.aggiungitodo=function(){
+        console.log($scope.newTodo);
+      }
 
+      $scope.cancellatodo=function(idtodo){
+        console.log("cancella"+idtodo);
+      }
+
+      $scope.eseguitodo=function(idtodo){
+        console.log("esegui"+idtodo);
+      }
+
+    function getTodos() {
+    Todos
+      .find()
+      .$promise
+      .then(function(results) {
+        $scope.todos = results;
+        console.log(results);
+      });
+  }
+  getTodos();
   }]);
 
